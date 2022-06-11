f7048850639ef7bf2ffe0cdb4651370151af7c61
mquan410 <95141427+mquan410@users.noreply.github.com>
2022-06-11 20:20:37 +0200

    fixed NewtonF2

--- src/interpreter/tmp.java
+++ src/interpreter/tmp.java
@@ -6,48 +6,46 @@ public class tmp {
 
     // An example function whose solution
     // is determined using Bisection Method.
-    // The function is x^5 + 5x^3 - 5
-    static double func(double x)
-    {
-//        return x * x * x - x * x + 2;
-        return (x * x * x * x * x) + (5 * (x * x * x)) - 5;
+
+    static double func(double x) {
+        // The function F1 is x^5 + 5x^3 - 5
+//        return (x * x * x * x * x) + (5 * (x * x * x)) - 5;
+        // The function F2 is 8x^6 + 3x^2 - 3
+        return (8 * (x * x * x * x * x * x)) + (3 * (x * x)) - 3;
     }
 
     // Derivative of the above function
-    // which is 5x^4 + 15x^2
 
-    static double derivFunc(double x)
-    {
-//        return 3 * x * x - 2 * x;
-        return (5 * (x * x * x * x)) + (15 * (x * x));
+    static double derivFunc(double x) {
+        // f1' is 5x^4 + 15x^2
+//        return (5 * (x * x * x * x)) + (15 * (x * x));
+        // f2' is 48x^5 + 6x
+        return (48 * (x * x * x * x * x)) + (6 * x);
     }
 
     // Function to find the root
-    static void newtonRaphson(double x)
-    {
+    static void newtonRaphson(double x) {
         double h = func(x) / derivFunc(x);
 
 
-        while (Math.abs(h) >= EPSILON)
-        {
+        while (Math.abs(h) >= EPSILON) {
+            System.out.println("f = " + func(x));
+            System.out.println("f' = " + derivFunc(x));
             h = func(x) / derivFunc(x);
+            System.out.println("value of h is: " + h);
 
             // x(i+1) = x(i) - f(x) / f'(x)
             x = x - h;
         }
 
-//        System.out.print("The value of the"
-//                + " root is : "
-//                + Math.round(x * 1000.0) / 1000.0);
         System.out.println("value is " + x);
     }
 
     // Driver code
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
 
         // Initial values assumed
-        double x0 = 1;
+        double x0 = 3;
         newtonRaphson(x0);
     }
 }
