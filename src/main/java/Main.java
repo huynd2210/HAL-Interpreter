de05fd9c82d89fd46d4058b5d149ec33565d711a
mquan410 <95141427+mquan410@users.noreply.github.com>
2022-07-13 14:42:09 +0200

    added run file HAL1961

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -73,6 +73,34 @@ public class Main {
     public static void main(String[] args) throws Exception {
 //        testPageTable(false);
         manualTest(false);
+//        runHAL1961(new String[]{"HAL1961", "false"});
+    }
+
+    private static void runHAL1961(String[] args) {
+        String file = "HAL1961";
+        if (args.length == 0 || args[0] == null || args[0].equalsIgnoreCase("")) {
+            System.out.println("File argument not found, defaulting to HAL1961");
+        } else {
+            file = args[0];
+        }
+
+        boolean isDebug = false;
+        if (args.length < 2) {
+            System.out.println("Debug argument not found, defaulting to false");
+        } else {
+            isDebug = Boolean.parseBoolean(args[1]);
+        }
+
+        System.out.println(file);
+        String program = readProgramFile(file);
+        System.out.println(program);
+
+        Interpreter interpreter = new Interpreter(1);
+        interpreter.addProgram("HALPrograms/" + file);
+        interpreter.run(isDebug);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press any key to exit");
+        sc.nextLine();
     }
 
     private static void praktikum2(String[] args) {
